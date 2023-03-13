890367a39c4e7a18ffc8f5201d2d6e13e6f97c08
Wasswa Enock Male <enockmale97@gmail.com>
2023-03-13 23:48:52 +0300

    I have changed more about my react application

--- part2-notes-part2-1/src/service/notes.js
+++ part2-notes-part2-1/src/service/notes.js
@@ -1,14 +1,14 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/notes';
+const baseUrl = 'http://localhost:3001/api/notes';
 
 const getAll = ()=>{
     const request = axios.get(baseUrl);
-    const nonExisting = {
-        id: 1000,
-        content: "This is not saved to server",
-        important: true,
-    }
-    return request.then(response => response.data.concat(nonExisting));
+    // const nonExisting = {
+    //     id: 1000,
+    //     content: "This is not saved to server",
+    //     important: true,
+    // }
+    return request.then(response => response.data);
 }
 
 const create = newObject =>{
